3de9404127409f383cd768b2a9c09636d8029f44
ivan770 <ivan@ivan770.me>
2024-11-22 14:19:12 -0500

    Expose more types as an external API

--- src/definitions/mod.rs
+++ src/definitions/mod.rs
@@ -5,7 +5,10 @@ mod validity_info;
 
 pub mod helpers;
 
-pub use device_key::{CoseKey, DeviceKeyInfo, KeyAuthorizations};
+pub use device_key::{
+    cose_key::{CoseKey, EC2Curve, OKPCurve, EC2Y},
+    DeviceKeyInfo, KeyAuthorizations,
+};
 pub use issuer_signed::{IssuerNamespaces, IssuerSigned, IssuerSignedItem, IssuerSignedItemBytes};
 pub use mso::{DigestAlgorithm, DigestId, DigestIds, Mso};
 pub use validity_info::ValidityInfo;
